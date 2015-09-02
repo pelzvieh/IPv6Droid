7a6b45ef54d6d17d982a3497788c7ad00ecbb3b6
Andreas Feldner <pelzi@flying-snail.de>
2015-09-03 00:43:27 +0200

    Corrected behaviour if autostart preference is *not* set.

--- IPv6Droid/src/main/java/de/flyingsnail/ipv6droid/android/BootReceiver.java
+++ IPv6Droid/src/main/java/de/flyingsnail/ipv6droid/android/BootReceiver.java
@@ -3,14 +3,22 @@ package de.flyingsnail.ipv6droid.android;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
+/**
+ * This provides a callback function, asking MainActivity to start the VPN tunnel when it receives a boot completed
+ * message.
+ */
 public class BootReceiver extends BroadcastReceiver {
     public BootReceiver() {
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+        SharedPreferences myPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) &&
+                myPrefs.getBoolean("autostart", false)) {
             Intent i = new Intent(context, MainActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             i.putExtra(MainActivity.EXTRA_AUTOSTART, true);
